ba222927197864be0771a9242ef4f43899e98336
Lucas Freitas <pro.lucasrfreitas@gmail.com>
2022-07-07 19:39:34 -0300

    start project

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
